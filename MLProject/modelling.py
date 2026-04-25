5f7ac68e05329b9ddc5d1b47ee4c5248251e24b1
MahfudAwal <mahfudawaludin.009@gmail.com>
2026-04-25 22:29:27 +0700

    push

---
+++ MLProject/modelling.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import argparse
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, r2_score
+
+import mlflow
+import mlflow.sklearn
+
+
+# ======================
+# 1. ARGUMENT (SUPPORT MLFLOW PROJECT)
+# ======================
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_path", type=str, default="preprocessed_data.csv")
+args = parser.parse_args()
+
+
+# ======================
+# 2. SET MLFLOW
+# ======================
+mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_experiment("Student_Performance_Model")
+
+# AUTLOG
+mlflow.autolog()
+
+
+# ======================
+# 3. LOAD DATA
+# ======================
+df = pd.read_csv(args.data_path)
+
+X = df.drop(columns=["Exam_Score"])
+y = df["Exam_Score"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+
+# ======================
+# 4. TRAINING
+# ======================
+with mlflow.start_run():
+
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    print("MAE:", mae)
+    print("R2 Score:", r2)
+
+    # ✅ Manual logging (biar muncul jelas di UI)
+    mlflow.log_metric("MAE", mae)
+    mlflow.log_metric("R2", r2)
+
+    # ✅ WAJIB: simpan model (biar Docker bisa build)
+    mlflow.sklearn.log_model(model, "model")
